2d70e85802a6349d5b9839aefda22b5976f03ed1
hangyu guo <3025943684@qq.com>
2026-04-18 15:07:07 +0800

    实现编辑资料页面前端

---
+++ frontend/src/views/user/profile/components/Username.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {ref, watch} from "vue";
+
+const props = defineProps(['username'])
+const myUsername = ref(props.username)
+
+watch(() => props.username, newVal => {
+  myUsername.value = newVal
+})
+
+defineExpose({
+  myUsername,
+})
+</script>
+
+<template>
+  <fieldset class="fieldest">
+    <label  class="label text-base">用户名</label>
+    <input v-model="myUsername" type="text" class="input w-108">
+  </fieldset>
+</template>
+
+<style scoped>
+
+</style>

744f70c5e2350099ac401e8523faf3041a1a45ea
hangyu guo <3025943684@qq.com>
2026-04-19 19:19:52 +0800

    添加长期记忆

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -15,6 +15,9 @@ from web.views.user.profile.update import UpdateProfileView
 from web.views.friend.get_list import GetListFriendView
 from web.views.friend.get_or_create import GetOrCreateFriendView
 from web.views.friend.remove import RemoveFriendView
+from web.views.friend.message.chat.chat import MessageChatView
+from web.views.friend.message.get_history import GetHistoryView
+
 
 urlpatterns = [
    path('api/user/account/login/',LoginView.as_view()),
@@ -33,6 +36,8 @@ urlpatterns = [
     path('api/friend/get_or_create/', GetOrCreateFriendView.as_view()),
     path('api/friend/remove/', RemoveFriendView.as_view()),
     path('api/friend/get_list/', GetListFriendView.as_view()),
+path('api/friend/message/chat/', MessageChatView.as_view()),
+path('api/friend/message/get_history/', GetHistoryView.as_view()),
     path('',index),
 
     re_path(r'^(?!media/|static/|assets/).*$', index)
